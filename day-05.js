60cbb8135ca63665c08d4c3ce747339c1eed6647
Sophii <89339936+Sophii100proc@users.noreply.github.com>
2022-01-27 12:13:39 +0200

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -4,11 +4,22 @@ function correctTail(body, tail){
   
    let sub = body.substr(body.length-(tail.length))
   
-  if (sub = tail) {
+  if (sub === tail) {
     return true
   }
   else 
     return false
   }
+
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+
+function bonusTime(salary, bonus) {
+if (bonus === true){
+return ("£" + salary * 10)
+} else {
+return ("£" + salary);
+}
+}
+bonusTime(1200, true);
 
 // 
