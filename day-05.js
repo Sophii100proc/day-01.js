936a9b6a350350fab178d72727f560fd2bc6c2d2
Sophii <89339936+Sophii100proc@users.noreply.github.com>
2022-01-27 09:47:27 +0200

    Create day-05.js

---
+++ day-05.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/is-this-my-tail/train/javascript
+
+function correctTail(body, tail){
+  
+   let sub = body.substr(body.length-(tail.length))
+  
+  if (sub = tail) {
+    return true
+  }
+  else 
+    return false
+  }
+
+// 
