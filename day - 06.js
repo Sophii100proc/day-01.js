ccbf44120f0703a98adeb9b94e3f3c5e82ca6c4f
Sophii <89339936+Sophii100proc@users.noreply.github.com>
2022-01-28 10:16:37 +0200

    Update day - 06.js

--- day - 06.js
+++ day - 06.js
@@ -5,4 +5,21 @@ function maxMultiple(divisor, bound){
     return (bound-n)
 }
 
-// 
+// https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+
+function unusualFive() {
+   const a = "Sofia";
+const b = a.length;
+return b
+}
+
+
+// https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+
+function hero(bullets, dragons) {
+  if (bullets >= dragons * 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
