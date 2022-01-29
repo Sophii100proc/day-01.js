e386b8fbfe8253df64a8d164320fe325c267b2f7
Sophii <89339936+Sophii100proc@users.noreply.github.com>
2022-01-29 15:36:17 +0200

    Update day - 06.js

--- day - 06.js
+++ day - 06.js
@@ -1,16 +1,16 @@
 // https://www.codewars.com/kata/maximum-multiple/train/javascript
 
-function maxMultiple(divisor, bound){
-    const n = (bound % divisor)
-    return (bound-n)
+function maxMultiple(divisor, bound) {
+  const result = bound % divisor;
+  return bound - result;
 }
 
 // https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
 
 function unusualFive() {
-   const a = "Sofia";
-const b = a.length;
-return b
+  const str = "Sofia";
+  const result = a.length;
+  return result;
 }
 
 
