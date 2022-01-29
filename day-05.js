6019e0e8679efec32788678f5b3deee2889b6026
Sophii <89339936+Sophii100proc@users.noreply.github.com>
2022-01-29 15:43:14 +0200

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -1,24 +1,22 @@
 // https://www.codewars.com/kata/is-this-my-tail/train/javascript
 
-function correctTail(body, tail){
-  
-   let sub = body.substr(body.length-(tail.length))
-  
-  if (sub === tail) {
-    return true
-  }
-  else 
-    return false
-  }
+function correctTail(body, tail) {
+  let getLastL = body.substr(body.length - tail.length);
+
+  if (getLastL === tail) {
+    return true;
+  } else return false;
+}
+
 
 // https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
 
 function bonusTime(salary, bonus) {
-if (bonus === true){
-return ("£" + salary * 10)
-} else {
-return ("£" + salary);
-}
+  if (bonus === true) {
+    return "£" + salary * 10;
+  } else {
+    return "£" + salary;
+  }
 }
 bonusTime(1200, true);
 
@@ -27,11 +25,11 @@ bonusTime(1200, true);
 automorphic(7);
 
 function automorphic(n) {
-  const a = Math.pow(n, 2);
-  const res = a.toString();
-  const b = n.toString();
+  const square = Math.pow(n, 2);
+  const sqToStr = square.toString();
+  const nToStr = n.toString();
 
-  if (res[res.length - b.length] === b) {
+  if (sqToStr[sqToStr.length - nToStr.length] === nToStr) {
     return "Automorphic";
   } else {
     return "Not!!";
