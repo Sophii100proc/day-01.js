155310127ae81a6f04395d0e07a19564d19e8dfc
Sophii <89339936+Sophii100proc@users.noreply.github.com>
2022-02-05 15:18:10 +0200

    Update day-11.js

--- day-11.js
+++ day-11.js
@@ -15,15 +15,3 @@ return "false"
 return "true"
 }
 }
-
-// https://www.codewars.com/kata/5729b103dd8bac11a900119e/train/javascript
-
-function fiveLine(s) {
-  let a = s.trim();
-  for (let i = 2; i < 10; i++) {
-    a += "\n" + s.trim().repeat(i);
-  }
-  return a;
-}
-
-// https://www.codewars.com/kata/57284d23e81185ae6200162a/train/javascript  - роблю
