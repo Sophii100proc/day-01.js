f3c51c6555064a135eb88de702e55a5395f22a89
Sophii <89339936+Sophii100proc@users.noreply.github.com>
2022-02-06 16:33:02 +0200

    Update day-12.js

--- day-12.js
+++ day-12.js
@@ -13,3 +13,25 @@ return (result);
 
 // https://www.codewars.com/kata/57f75cc397d62fc93d000059/train/javascript
 
+
+function calc() {
+  const a = "abd";
+  const b = a.split("");
+
+  let result = b.map((el) => {
+    return el.charCodeAt();
+  });
+  let c = result.join("").split("");
+
+  let total1 = 0;
+  let total2 = 0;
+
+  for (let i = 0; i < c.length; i++) {
+    if (c[i] !== "7") {
+      total1 + i;
+    } else if (c[i] === 7) {
+      total2 + i;
+    }
+  }
+}
+console.log(calc());
