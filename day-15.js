269f542007cf434d112b624f473ac6bc7c1fcc5e
Sophii <89339936+Sophii100proc@users.noreply.github.com>
2022-02-10 22:27:06 +0200

    Update day-15.js

--- day-15.js
+++ day-15.js
@@ -8,4 +8,20 @@ const shuffleIt = (arr, ...arr1) => {
 };
 
 
-//https://www.codewars.com/kata/5fd8aa5743b49e0012d43e50/train/javascript
+//https://www.codewars.com/kata/5fd8aa5743b49e0012d43e50/train/javascript - не виходить до кінця
+
+function loopArr(arr, direction, steps) {
+  for (let i = 0; i <= steps; i++) {
+    if (direction === "left") {
+      arr.shift(arr[i]);
+      arr.push(arr[i]);
+    } else {
+      arr.pop(arr[i]);
+      arr.unshift(arr[i]);
+    }
+  }
+  return arr;
+}
+loopArr([2, 4, 6, 7, 8, 9], "right", 2);
+
+//
