6fcaad2af740712b6f330a8675e1718e3f414094
Sophii <89339936+Sophii100proc@users.noreply.github.com>
2022-02-19 15:16:12 +0200

    Create day-27.js

---
+++ day-27.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/shifty-closures/train/javascript
+
+const greet_abe = function() {
+  const name = 'Abe';
+  return "Hello, " + name + '!';
+};
+ 
+const greet_ben = function() {
+  const name = 'Ben';
+  return "Hello, " + name + '!';
+};
+
+
