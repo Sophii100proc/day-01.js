b7695e532a3f2c07ac8a2683e9f610b4c13dd04c
Sophii <89339936+Sophii100proc@users.noreply.github.com>
2022-02-19 15:07:30 +0200

    Update day-25.js

--- day-25.js
+++ day-25.js
@@ -4,3 +4,17 @@ Function.prototype.call = function() {
   var arg = Array.prototype.slice.apply(arguments,[1]);
   return this.apply(arguments[0], arg);
 }; 
+
+
+// https://www.codewars.com/kata/53d4eec01f1a9b3020000786/train/javascript
+
+var name = 'The Window';
+
+var alpha = {
+    name : 'My Alpha',
+    getNameFunc : function(){
+    return function(){
+        return this.name;
+        }.bind(this);
+    }
+};
