daf0a2a8ecc075489588ad9eebd68056013f605c
Sophii <89339936+Sophii100proc@users.noreply.github.com>
2022-02-19 14:46:15 +0200

    Create day-23.js

---
+++ day-23.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5258b272e6925db09900386a/train/javascript
+
+function numbers(a, b) {
+if (!isNaN(a) && !isNaN(b)){
+    return true;
+} else {
+    return false;
+}
+}
+    
+numbers(2, 3);
